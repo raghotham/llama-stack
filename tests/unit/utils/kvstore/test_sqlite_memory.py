5227448d6ffe8fdda9b9e561c7b5db25b89d3c68
Raghotham Murthy <rsm@meta.com>
2025-10-04 18:20:18 -0700

    fix pre-commit error

--- tests/unit/utils/kvstore/test_sqlite_memory.py
+++ tests/unit/utils/kvstore/test_sqlite_memory.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
-import pytest
 
 from llama_stack.providers.utils.kvstore.sqlite.config import SqliteKVStoreConfig
 from llama_stack.providers.utils.kvstore.sqlite.sqlite import SqliteKVStoreImpl
